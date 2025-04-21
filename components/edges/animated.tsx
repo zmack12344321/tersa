2931e340fccdcc2d325140c7b6de8e9dd4552ec3
Hayden Bleasel <hello@haydenbleasel.com>
2025-04-21 10:54:38 -0700

    Offset edges

--- components/edges/animated.tsx
+++ components/edges/animated.tsx
@@ -1,26 +1,139 @@
-import { BaseEdge, type EdgeProps, getBezierPath } from '@xyflow/react';
+import {
+  BaseEdge,
+  type EdgeProps,
+  type InternalNode,
+  type Node,
+  getBezierPath,
+  useInternalNode,
+} from '@xyflow/react';
+import { Position } from '@xyflow/react';
+
+const getParams = (
+  nodeA: InternalNode<Node>,
+  nodeB: InternalNode<Node>
+): [number, number, Position] => {
+  const centerA = getNodeCenter(nodeA);
+  const centerB = getNodeCenter(nodeB);
+
+  const horizontalDiff = Math.abs(centerA.x - centerB.x);
+  const verticalDiff = Math.abs(centerA.y - centerB.y);
+
+  let position: Position;
+
+  // when the horizontal difference between the nodes is bigger, we use Position.Left or Position.Right for the handle
+  if (horizontalDiff > verticalDiff) {
+    position = centerA.x > centerB.x ? Position.Left : Position.Right;
+  } else {
+    // here the vertical difference between the nodes is bigger, so we use Position.Top or Position.Bottom for the handle
+    position = centerA.y > centerB.y ? Position.Top : Position.Bottom;
+  }
+
+  const [x, y] = getHandleCoordsByPosition(nodeA, position);
+
+  return [x, y, position];
+};
+
+const getHandleCoordsByPosition = (
+  node: InternalNode<Node>,
+  handlePosition: Position
+) => {
+  // all handles are from type source, that's why we use handleBounds.source here
+  const handle = node.internals.handleBounds?.source?.find(
+    (h) => h.position === handlePosition
+  );
+
+  if (!handle) {
+    throw new Error(`Handle not found for position: ${handlePosition}`);
+  }
+
+  let offsetX = handle.width / 2;
+  let offsetY = handle.height / 2;
+
+  // this is a tiny detail to make the markerEnd of an edge visible.
+  // The handle position that gets calculated has the origin top-left, so depending which side we are using, we add a little offset
+  // when the handlePosition is Position.Right for example, we need to add an offset as big as the handle itself in order to get the correct position
+  switch (handlePosition) {
+    case Position.Left:
+      offsetX = 0;
+      break;
+    case Position.Right:
+      offsetX = handle.width;
+      break;
+    case Position.Top:
+      offsetY = 0;
+      break;
+    case Position.Bottom:
+      offsetY = handle.height;
+      break;
+    default:
+      throw new Error(`Invalid handle position: ${handlePosition}`);
+  }
+
+  const x = node.internals.positionAbsolute.x + handle.x + offsetX;
+  const y = node.internals.positionAbsolute.y + handle.y + offsetY;
+
+  return [x, y];
+};
+
+const getNodeCenter = (node: InternalNode<Node>) => {
+  if (!node.measured.width || !node.measured.height) {
+    throw new Error('Node has no measured dimensions');
+  }
+
+  return {
+    x: node.internals.positionAbsolute.x + node.measured.width / 2,
+    y: node.internals.positionAbsolute.y + node.measured.height / 2,
+  };
+};
+
+const getEdgeParams = (
+  source: InternalNode<Node>,
+  target: InternalNode<Node>
+) => {
+  const [sx, sy, sourcePos] = getParams(source, target);
+  const [tx, ty, targetPos] = getParams(target, source);
+
+  return {
+    sx,
+    sy,
+    tx,
+    ty,
+    sourcePos,
+    targetPos,
+  };
+};
 
 export const AnimatedEdge = ({
   id,
-  sourceX,
-  sourceY,
-  targetX,
-  targetY,
-  sourcePosition,
-  targetPosition,
+  source,
+  target,
+  markerEnd,
+  style,
 }: EdgeProps) => {
+  const sourceNode = useInternalNode(source);
+  const targetNode = useInternalNode(target);
+
+  if (!sourceNode || !targetNode) {
+    return null;
+  }
+
+  const { sx, sy, tx, ty, sourcePos, targetPos } = getEdgeParams(
+    sourceNode,
+    targetNode
+  );
+
   const [edgePath] = getBezierPath({
-    sourceX,
-    sourceY,
-    sourcePosition,
-    targetX,
-    targetY,
-    targetPosition,
+    sourceX: sx,
+    sourceY: sy,
+    sourcePosition: sourcePos,
+    targetX: tx,
+    targetY: ty,
+    targetPosition: targetPos,
   });
 
   return (
     <>
-      <BaseEdge id={id} path={edgePath} />
+      <BaseEdge id={id} path={edgePath} markerEnd={markerEnd} style={style} />
       <circle r="10" fill="var(--primary)">
         <animateMotion dur="2s" repeatCount="indefinite" path={edgePath} />
       </circle>
