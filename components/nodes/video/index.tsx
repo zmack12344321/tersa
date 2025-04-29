6047c4468da090c0870b67c1138f2ed4732b9366
Hayden Bleasel <hello@haydenbleasel.com>
2025-04-29 12:17:34 -0700

    Update index.tsx

--- components/nodes/video/index.tsx
+++ components/nodes/video/index.tsx
@@ -5,6 +5,10 @@ export type VideoNodeProps = {
   type: string;
   data: {
     source: 'primitive' | 'transform';
+    content?: {
+      url: string;
+      type: string;
+    };
     generated?: {
       url: string;
       type: string;
