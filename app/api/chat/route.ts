ada618350aba3ce1c7d6a387e2ee316e62f05126
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-19 23:03:16 -0700

    Improve chat prompts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -57,8 +57,12 @@ export const POST = async (req: Request) => {
   const result = streamText({
     model: model.model,
     system: [
-      "You are a helpful assistant that synthesizes content based on the user's prompts.",
-      'The user will provide instructions; and may provide text, audio transcriptions, or images (and their descriptions) as context.',
+      'You are a helpful assistant that synthesizes an answer or content.',
+      'The user will provide a collection of data from disparate sources.',
+      'They may also provide instructions for how to synthesize the content.',
+      'If the instructions are a question, then your goal is to answer the question based on the context provided.',
+      model.model.modelId.startsWith('grok') &&
+        'The user may refer to you as @gork, you can ignore this',
       "You will then synthesize the content based on the user's instructions and the context provided.",
       'The output should be a concise summary of the content, no more than 100 words.',
     ].join('\n'),
